31235c4352dc02342fe3593c77675284fabe5563
Fa3ris <Fa3ris@users.noreply.github.com>
2020-08-29 15:32:57 +0200

    backup previous sync

--- sync2gdrive.ps1
+++ sync2gdrive.ps1
@@ -1,6 +1,14 @@
+# directory to sync
 $dir = "$HOME/Documents/rclone-test"
-$path = "googleDrive:backup"
 
-Write-Output "sync folder $dir to gdrive"
+# remote directory to sync
+$pathCurrent = "googleDrive:backup/current"
 
-rclone.exe sync $dir $path --progress -vvvvv --log-file "$HOME/Documents/rclone.log"
+$saveDate = Get-Date -Format "yyyyMMddHHmmss"
+
+# remote directory to copy files modified/deleted/renamed by sync 
+$pathSave = "googleDrive:backup/old/$saveDate"
+
+Write-Output "sync folder $dir to gdrive in $pathCurrent and saving previous backup to $pathSave"
+
+rclone.exe sync $dir $pathCurrent --progress -vvvvv --log-file "$HOME/Documents/rclone.log" --backup-dir $pathSave
